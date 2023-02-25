e9947dde42501bacd31535ee524e25d318a3f3dc
LamaPirat <g.skrefsrud@gmail.comg.skrefsrud@gmail.com>
2023-02-25 16:54:34 +0100

    new task complete counter almost done, next: make it transitionable when switching lists

---
+++ src/taskPrecent-render.js
@@ -0,0 +1,44 @@
+const number = document.getElementById("number");
+const skillContainer = document.querySelector(".skill");
+const circle = document.getElementById("circle");
+var lastCount = 0;
+circle.setAttribute("stroke-dashoffset", 0);
+number.innerHTML = `${0}%`;
+
+export function setCircle(prec) {
+  let precent = (1 - prec).toFixed(1);
+  let counter = lastCount;
+  //   skillContainer.style.setProperty(
+  //     "--task-precentage-from",
+  //     `${counter / 100}`
+  //   );
+  //   skillContainer.style.setProperty("--task-precentage-to", `${precent}`);
+
+  circle.setAttribute("stroke-dashoffset", 450 - 450 * precent);
+
+  if (precent > counter / 100) {
+    setInterval(() => {
+      if (counter == precent * 100) {
+        clearInterval;
+        return;
+      } else {
+        counter += 1;
+        number.innerHTML = `${counter}%`;
+        lastCount = counter;
+      }
+    }, 10);
+  } else if (precent < counter / 100) {
+    setInterval(() => {
+      if (counter == precent * 100) {
+        clearInterval;
+        return;
+      } else {
+        counter -= 1;
+        number.innerHTML = `${counter}%`;
+        lastCount = counter;
+      }
+    }, 10);
+  } else {
+    return;
+  }
+}
